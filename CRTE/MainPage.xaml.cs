d35269ced7f53048cc08de0656d8698b672df7f5
Christian Widjaya <Christian.surya.widjaya@gmail.com>
2018-01-06 23:04:10 +0700

    push

--- CRTE/MainPage.xaml.cs
+++ CRTE/MainPage.xaml.cs
@@ -79,7 +79,7 @@ namespace CRTE
                     dynamic res =
                         await RequestFromAPI("addOnlineUser", "username=" + username + "&channel=" + chatcode);
                     Debug.WriteLine((string)res.message);
-                    
+
                     ConnectToORTC();
                     await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
                         () =>
@@ -106,12 +106,12 @@ namespace CRTE
             ortcClient.Connect("jXNrX9", "token");
             Message message = new Message();
             message.sentAtDate = DateTime.Now.ToLocalTime().ToString("dd/MM/yyyy");
-            if(message.sentAtDate != null)
+            if (message.sentAtDate != null)
             {
                 TxtChat.Text = message.sentAtDate;
                 TxtSend.IsEnabled = true;
             }
-            
+
 
         }
 
@@ -133,7 +133,7 @@ namespace CRTE
             {
                 Debug.WriteLine(ex);
             }
-            
+
         }
 
         async Task<dynamic> RequestFromAPI(string cmds, string param)
@@ -162,7 +162,7 @@ namespace CRTE
             //myID = myDictionary["username"].ToString();
             myID = username;
             Debug.WriteLine(myID);
-            
+
         }
 
 
@@ -184,7 +184,7 @@ namespace CRTE
 
             Message parsedMessage = JsonConvert.DeserializeObject<Message>(message);
             TxtChat.Text += "\n" + parsedMessage.id + " (" + parsedMessage.sentAt + "): " + parsedMessage.text;
-            
+
 
             // check if message is from another user
             if (!parsedMessage.id.Equals(myID))
@@ -193,7 +193,7 @@ namespace CRTE
             }
         }
 
-        private async void SendMessage(object sender, KeyRoutedEventArgs e)
+        private void SendMessage(object sender, KeyRoutedEventArgs e)
         {
             if (e.Key == VirtualKey.Enter)
             {
@@ -214,7 +214,7 @@ namespace CRTE
                     Debug.WriteLine("Sending message: " + jsonMessage);
                     ortcClient.Send(chatcode, jsonMessage);
                     TxtSend.Text = "";
-                   
+
                     MyScrollViewer.UpdateLayout();
                     //MyScrollViewer.ChangeView(0.0f, double.MaxValue, 1.0f);
                     MyScrollViewer.ScrollToVerticalOffset(MyScrollViewer.ScrollableHeight);
@@ -232,169 +232,3 @@ namespace CRTE
         }
     }
 }
-/*
-
-
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
-public sealed partial class MainPage : Page
-    {
-        string ipServer = "";
-        string portServer = "8888";
-        string username = "";
-        string publicip = "";
-        
-        public MainPage()
-        {
-            this.InitializeComponent();
-           // Debug.WriteLine(username);
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            //username = e.Parameter as string;
-            Dictionary<string, string> myDictionary = new Dictionary<string, string>();
-            myDictionary = e.Parameter as Dictionary<string, string>;
-            username = myDictionary["username"].ToString();
-            Debug.WriteLine(username);
-            publicip = myDictionary["publicip"].ToString();
-            Debug.WriteLine(publicip);
-        }
-
-        private string GetLocalIp()
-        {
-            var icp = NetworkInformation.GetInternetConnectionProfile();
-            if (icp?.NetworkAdapter == null) return null;
-            var hostname = NetworkInformation.GetHostNames().SingleOrDefault(hn =>
-           hn.IPInformation?.NetworkAdapter != null && hn.IPInformation.NetworkAdapter.NetworkAdapterId == icp.NetworkAdapter.NetworkAdapterId);
-            //the ip address
-            return hostname?.CanonicalName;
-        }
-
-        private async void TextingChat(object sender, KeyRoutedEventArgs e)
-        {
-            if (e.Key == VirtualKey.Enter)
-            {
-                // the user has pressed enter, do something here
-                try
-                {
-                    Debug.WriteLine("[SENDING] Create StreamSocket and Establish Connection...");
-                    Windows.Networking.Sockets.StreamSocket socket = new Windows.Networking.Sockets.StreamSocket();
-
-                    Windows.Networking.HostName serverHost = new Windows.Networking.HostName(ipServer);
-
-                    Debug.WriteLine("[SENDING] Prepare to Connect...");
-                    string serverPort = portServer;
-                    Debug.WriteLine("[SENDING] Connecting...");
-                    await socket.ConnectAsync(serverHost, serverPort);
-                    Debug.WriteLine("[SENDING] Connected");
-
-                    Debug.WriteLine("[SENDING] Prepare Output Data...");
-                    Stream streamOut = socket.OutputStream.AsStreamForWrite();
-                    StreamWriter writer = new StreamWriter(streamOut);
-                    string request = username+"#CHAT#"+TxtSend.Text;
-                    await writer.WriteLineAsync(request);
-                    await writer.FlushAsync();
-                    Debug.WriteLine("[SENDING] Read Feedback from server...");
-                    Stream streamIn = socket.InputStream.AsStreamForRead();
-                    StreamReader reader = new StreamReader(streamIn);
-
-                    char[] result = new char[2048];
-                    await reader.ReadAsync(result, 0, 2048);
-                    string s = new string(result);
-                    Debug.WriteLine("[Sending] Done Data From server..." + s);
-                }
-                catch (Exception x)
-                {
-
-                }
-            }
-        }
-        private async Task StartListeningAsync()
-        {
-            try
-            {
-                Debug.WriteLine("[LISTENING] Creating Stream Socket...");
-                StreamSocketListener socketListener = new StreamSocketListener();
-
-                socketListener.ConnectionReceived += SocketListener_ConnectionReceived;
-                Debug.WriteLine("[LISTENING] Create Event Handler...");
-
-                await socketListener.BindServiceNameAsync(portServer);
-                Debug.WriteLine("[LISTENING] Start Listening");
-            }
-            catch (Exception e)
-            {
-                //hand
-            }
-        }
-        private async void SocketListener_ConnectionReceived(StreamSocketListener sender,
-            StreamSocketListenerConnectionReceivedEventArgs args)
-        {
-            Debug.WriteLine("[LISTENING] Incomming Connection...");
-            Stream inStream = args.Socket.InputStream.AsStreamForRead();
-            StreamReader reader = new StreamReader(inStream);
-            string request = await reader.ReadLineAsync();
-            Debug.WriteLine("[LISTENING] Incomming data is " + request);
-
-            string TxtData="";
-
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                TxtData += "From IP " + args.Socket.Information.RemoteAddress + " :\r\n " + request + "\r\n";
-                TxtSend.Text = "";
-            });
-
-            Debug.WriteLine("[LISTENING] Sending Feed Back");
-            Stream outStream = args.Socket.OutputStream.AsStreamForWrite();
-            StreamWriter writer = new StreamWriter(outStream);
-            char[] bytes = null;
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                request = (TxtData);
-                bytes = request.ToCharArray();
-            });
-            await writer.WriteAsync(bytes, 0, bytes.Length);
-            await writer.FlushAsync();
-            Debug.WriteLine("[LISTENING] Done..." + bytes.Length);
-        }
-
-        private async void TextBoxTextChanged(object sender, TextChangedEventArgs e)
-        {
-            try
-            {
-                Debug.WriteLine("[SENDING] Create StreamSocket and Establish Connection...");
-                Windows.Networking.Sockets.StreamSocket socket = new Windows.Networking.Sockets.StreamSocket();
-
-                Windows.Networking.HostName serverHost = new Windows.Networking.HostName(ipServer);
-
-                Debug.WriteLine("[SENDING] Prepare to Connect...");
-                string serverPort = portServer;
-                Debug.WriteLine("[SENDING] Connecting...");
-                await socket.ConnectAsync(serverHost, serverPort);
-                Debug.WriteLine("[SENDING] Connected");
-
-                Debug.WriteLine("[SENDING] Prepare Output Data...");
-                Stream streamOut = socket.OutputStream.AsStreamForWrite();
-                StreamWriter writer = new StreamWriter(streamOut);
-                string request = username + "#DATA#" + TxtCoop.Text;
-                await writer.WriteLineAsync(request);
-                await writer.FlushAsync();
-                Debug.WriteLine("[SENDING] Read Feedback from server...");
-                Stream streamIn = socket.InputStream.AsStreamForRead();
-                StreamReader reader = new StreamReader(streamIn);
-
-                char[] result = new char[2048];
-                await reader.ReadAsync(result, 0, 2048);
-                string s = new string(result);
-                Debug.WriteLine("[Sending] Done Data From server..." + s);
-            }
-            catch (Exception x)
-            {
-
-            }
-        }
-    }
-}
-*/
